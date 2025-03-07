d9b200791794bf4ea5059a425216c9a0303c0b9c
Jonathan Marvel <nathannnms04@gmail.com>
2025-03-07 19:30:10 +0700

    Menyelesaikan isi tabel dan melakukan migration serta mengecek statusnya

---
+++ database/migrations/2025_03_07_102151_create_details_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('details', function (Blueprint $table) {
+            $table->bigIncrements('detail_id');
+            $table->unsignedBigInteger('penjualan_id');
+            $table->unsignedBigInteger('produk_id');
+            $table->decimal('informasi_harga');
+            $table->integer('jumlah_produk');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('details');
+    }
+};
